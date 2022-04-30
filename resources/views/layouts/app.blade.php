86e6d54504e667e198470e91b0506a5358243e09
Aaron lawrence <91675986+AaronNeonDigital@users.noreply.github.com>
2022-04-30 22:08:10 +0100

    Added google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -142,6 +142,16 @@
 
     @livewireScripts
     <script src="{{  mix('js/app.js')  }}"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J1ZVVVL421"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-J1ZVVVL421');
+    </script>
 </body>
 
 </html>
